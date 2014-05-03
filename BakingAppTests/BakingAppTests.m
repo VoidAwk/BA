97d8ec17dc5b985309ae89b77fe537609f99f274
Joe Ilagan <joeilagan@Joes-MacBook-Pro-2.local>
2014-05-03 18:13:20 +0800

    Initial Commit

---
+++ BakingAppTests/BakingAppTests.m
@@ -0,0 +1,34 @@
+//
+//  BakingAppTests.m
+//  BakingAppTests
+//
+//  Created by Joe Ilagan on 5/3/14.
+//  Copyright (c) 2014 Void Awk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BakingAppTests : XCTestCase
+
+@end
+
+@implementation BakingAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
