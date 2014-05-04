4cd1bb5284052cb0c1403314de7a037b8b8a9b5a
Joe Ilagan <joeilagan@Joes-MacBook-Pro-2.local>
2014-05-04 13:32:42 +0800

    Initial Commit

---
+++ BakingApp/JCalculator.h
@@ -0,0 +1,13 @@
+//
+//  JCalculator.h
+//  BakingApp
+//
+//  Created by Joe Ilagan on 5/4/14.
+//  Copyright (c) 2014 Void Awk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCalculator : NSObject
+
+@end
---
+++ BakingApp/JCalculator.m
@@ -0,0 +1,13 @@
+//
+//  JCalculator.m
+//  BakingApp
+//
+//  Created by Joe Ilagan on 5/4/14.
+//  Copyright (c) 2014 Void Awk. All rights reserved.
+//
+
+#import "JCalculator.h"
+
+@implementation JCalculator
+
+@end
